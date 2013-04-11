fae423cbc5429dc4e6b703e33ac21be72f782ffc
ben monro <ben.monro@gmail.com>
2013-04-11 01:59:18 -0700

    check for null routers

--- chill.js
+++ chill.js
@@ -13,10 +13,29 @@ module.exports = function (app) {
     this.handle = function (route) {
 
         if(route.path) {
-            app.get(route.path, route.list);
-            app.post("/forms", route.create);
-            app.put("/forms", route.update);
-            app.delete("/forms", route.clearAll);
+            const ID_PARAM = "/:id?";
+
+            if (route.list) {
+                app.get(route.path, route.list);
+            }
+
+            if (route.findById) {
+                app.get(route.path + "/:id", route.findById);
+            }
+
+
+            if (route.create) {
+                app.post(route.path + ID_PARAM, route.create);
+            }
+
+            if(route.update) {
+            app.put(route.path + ID_PARAM, route.update);
+            }
+
+
+            if (route.delete) {
+                app.delete(route.path + ID_PARAM, route.delete);
+            }
         }
     }
 
